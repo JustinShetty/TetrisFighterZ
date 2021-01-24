9259d78ed0ca192fb7c1c9059e976184a43a57a3
Justin Shetty <jas0504@gmail.com>
2021-01-24 14:44:52 -0500

    pause menu lets you reset or quit

--- Main.gd
+++ Main.gd
@@ -2,8 +2,6 @@ extends Node
 
 export (PackedScene) var StartPauseMenu
 
-onready var iface = $UserInterface.get_node('UserInterface')
-
 var menu
 
 func _ready():
@@ -15,12 +13,11 @@ func _process(_delta):
 func new_game():
 	get_tree().reload_current_scene()
 
-func _input(event):
-	if event.is_action_pressed('reset'):
-		new_game()
+func reset_signal_handler():
+	new_game()
 
 func player_lives_updated():
-	iface.get_node('LivesLabel').text = str($Player.lives)
+	$UserInterface/UserInterface/LivesLabel.text = str($Player.lives)
 	if $Player.lives == 0:
 		new_game()
 
@@ -32,4 +29,4 @@ func _on_Player_updated_move_state():
 		state_str = 'WALK'
 	elif $Player.move_state == $Player.ms.DASH:
 		state_str = 'DASH'
-	iface.get_node('MoveStateLabel').text = state_str
+	$UserInterface/UserInterface/MoveStateLabel.text = state_str
