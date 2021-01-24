68efa591b40f6f47871b4f73e67dcbb5fddbbe8a
Justin Shetty <jas0504@gmail.com>
2021-01-24 18:08:18 -0500

    state machine refactor

--- Main.gd
+++ Main.gd
@@ -16,26 +16,5 @@ func _ready():
 	# for id in Input.get_connected_joypads():
 	#	print(Input.get_joy_name(id))
 
-func _process(_delta):
-	pass
-
-func new_game():
-	get_tree().reload_current_scene()
-
 func reset_signal_handler():
-	new_game()
-
-func player_lives_updated():
-	$UserInterface/UserInterface/LivesLabel.text = str($Player.lives)
-	if $Player.lives == 0:
-		new_game()
-
-func _on_Player_updated_move_state():
-	var state_str = ''
-	if $Player.move_state == $Player.ms.IDLE:
-		state_str = 'IDLE'
-	elif $Player.move_state == $Player.ms.WALK:
-		state_str = 'WALK'
-	elif $Player.move_state == $Player.ms.DASH:
-		state_str = 'DASH'
-	$UserInterface/UserInterface/MoveStateLabel.text = state_str
+	get_tree().reload_current_scene()
--- player/Player.gd
+++ player/Player.gd
@@ -1,28 +1,41 @@
 extends KinematicBody2D
 
-signal updated_move_state
-signal lives_updated
+# state machine
+enum states{IDLE, CROUCH, WALK, WALK_END, 
+DASH_START, DASH, DASH_END, PUNCH}
+var state_to_str = {states.IDLE: 'IDLE',
+					states.CROUCH: 'WALK_START',
+					states.WALK: 'WALK',
+					states.WALK_END: 'WALK_END',
+					states.DASH_START: 'DASH_START',
+					states.DASH: 'DASH',
+					states.DASH_END: 'DASH_END',
+					states.PUNCH: 'PUNCH'}
+var pstate = null
 
+# general movement
 var velocity = Vector2.ZERO
+
+# vertical movement
 const JUMP_IMPULSE = 1000
 const GRAVITY = 2000
 
-var LR_BUFFER_SIZE = 3
-var x_buffer = []
+# horizontal movement
+var STICK_BUFFER_SIZE = 3
+var stick_buffer = []
 const SPEED = 350
 const DASH_SPEED = 700
-const HARD_INP_THRESHOLD = 0.7
+const HEAVY_INP_THRESHOLD = 0.7
 
-enum ms{IDLE, WALK, DASH}
-var move_state = ms.IDLE
+#jumps
 const MAX_JUMPS = 2
 var jumps_remaining = MAX_JUMPS
 
+# spawning and lives
+var player_index = null
 const MAX_LIVES = 3
 var spawn_loc
 var lives
-
-var player_index = null
 
 var animation_state_machine
 
@@ -31,13 +44,13 @@ func local_action_name(a: String) -> String:
 
 func _ready():
 	animation_state_machine = $AnimationTree.get('parameters/playback')
-	for _i in range(LR_BUFFER_SIZE):
-		x_buffer.append(0)
+	set_state_and_animation(states.IDLE)
+	for _i in range(STICK_BUFFER_SIZE):
+		stick_buffer.append(Vector2())
 	hide()
 
 func set_lives(n):
 	lives = n
-	emit_signal('lives_updated')
 	
 func start(idx, pos):
 	player_index = idx
@@ -57,58 +70,56 @@ func _input(event):
 	elif event.is_action_pressed(local_action_name('attack')):
 		animation_state_machine.travel('punch')
 
-var input_vector = Vector2.ZERO
+func set_state_and_animation(state):
+	pstate = state
+	animation_state_machine.travel(state_to_str[state])
 
+var input_vector = Vector2.ZERO
 func process_input() -> void:
 	input_vector.x = Input.get_action_strength(local_action_name('move_right'))
 	input_vector.x -= Input.get_action_strength(local_action_name('move_left'))
 	input_vector.y = Input.get_action_strength(local_action_name('move_down'))
 	# print(str(player_index) + '_' + str(input_vector))
 
-	x_buffer.append(input_vector.x)
-	x_buffer.pop_front()
+	stick_buffer.append(input_vector)
+	stick_buffer.pop_front()
 
-	var earliest_is_centered = x_buffer.front() == 0
-	# var earliest_is_light = abs(x_buffer.front()) < HARD_INP_THRESHOLD and abs(x_buffer.front()) > 0
-	# var earliest_is_heavy = abs(x_buffer.front()) >= HARD_INP_THRESHOLD
-	var latest_is_centered = x_buffer.back() == 0
-	var latest_is_light = abs(x_buffer.back()) < HARD_INP_THRESHOLD and abs(x_buffer.back()) > 0
-	var latest_is_heavy = abs(x_buffer.back()) >= HARD_INP_THRESHOLD
+	# left/right
+	var earliest = stick_buffer.front().x
+	var latest = stick_buffer.back().x
+	var earliest_is_centered = earliest == 0
+	# var earliest_is_light = abs(earliest =) < H_INP_THRESHOLD and abs(earliest =) > 0
+	# var earliest_is_heavy = abs(earliest =) >= HEAVY_INP_THRESHOLD
+	var latest_is_centered = latest == 0
+	var latest_is_light = abs(latest) < HEAVY_INP_THRESHOLD and abs(latest) > 0
+	var latest_is_heavy = abs(latest) >= HEAVY_INP_THRESHOLD
 	
 	var dash_ok = earliest_is_centered and latest_is_heavy and is_on_floor()
 
-	match move_state:
-		ms.IDLE:
+	match pstate:
+		states.IDLE:
 			if dash_ok:
-				move_state = ms.DASH
-				animation_state_machine.travel('run_cycle')
+				set_state_and_animation(states.DASH)
 			elif latest_is_light or latest_is_heavy:
-				move_state = ms.WALK
-				animation_state_machine.travel('walk_cycle')
-		ms.WALK:
+				set_state_and_animation(states.WALK)
+		states.WALK:
 			if dash_ok:
-				move_state = ms.DASH
-				animation_state_machine.travel('run_cycle')
-			elif latest_is_centered:
-				move_state = ms.IDLE
-				animation_state_machine.travel('idle')
-		ms.DASH:
+				set_state_and_animation(states.DASH)
+		states.DASH:
 			if latest_is_light:
-				move_state = ms.WALK
-				animation_state_machine.travel('walk_cycle')
-			elif latest_is_centered:
-				move_state = ms.IDLE
-				animation_state_machine.travel('idle')
-
-	emit_signal('updated_move_state')
+				set_state_and_animation(states.WALK)
+		_:
+			print('unknown current state')
+	if latest_is_centered:
+		set_state_and_animation(states.IDLE)
 	
 func _physics_process(_delta):
 	process_input()	
 
 func _process(delta):
-	if move_state == ms.DASH:
+	if pstate == states.DASH:
 		velocity.x = input_vector.normalized().x * DASH_SPEED
-	elif move_state == ms.WALK:
+	elif pstate == states.WALK:
 		velocity.x = input_vector.x * SPEED
 	else:
 		velocity.x -= velocity.x * 0.2
